140c00d04d4519a024c52468695282bbe9d9e329
Ian Ro <ianrogers2422@gmail.com>
2021-07-29 20:22:29 -0400

    Wiring fixed

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 //Get Relevant Divs
 const input = document.querySelector(".info")
 const button =document.querySelector(".button")
-const ul = document.querySelector(".list-item")
+const list = document.querySelector(".list-item")
 //Event Listeners
 
 
 button.addEventListener("click", addDiv);
+
 //Functions
 
-function addDiv(e){
-  e.preventDefault();
+function addDiv(event){
+    event.preventDefault();
   //create div
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
@@ -19,5 +20,12 @@ function addDiv(e){
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
   input.value = "";
+  //create buttons
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML= '<button> X </button>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  list.appendChild(todoDiv);
+
 }
 
