cd95f887be13194d135e8242c663238804633a7c
Ian Ro <ianrogers2422@gmail.com>
2021-07-30 23:19:59 -0400

    Delete button failed attempt

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const list = document.querySelector(".list-item")
 
 
 button.addEventListener("click", addDiv);
+list.addEventListener("click", deleteCheck);
 
 //Functions
-
 function addDiv(event){
     event.preventDefault();
   //create div
@@ -22,10 +22,25 @@ function addDiv(event){
   input.value = "";
   //create buttons
   const completedButton = document.createElement("button");
-  completedButton.innerHTML= '<button> X </button>';
+  completedButton.innerHTML= '<button> add </button>';
   completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
+  //Delete Button
+  const Delete = document.createElement("button");
+  Delete.innerHTML = '<button> X </button>';
+  Delete.classList.add("delete-button");
+  todoDiv.appendChild(Delete);
+  //Last Todo
   list.appendChild(todoDiv);
+  input.value = "";
 
 }
 
+function deleteCheck (event) {
+  const item = event.target;
+  //Delete
+  if (item.classList[0] === "delete-button"){
+    const todoRem = item.parentElement;
+    todoRem.remove();
+  }
+}
